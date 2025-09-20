e6f302b9a34df41f7291ef26946795b4a5ec2d88
Waren Gonzaga <github@warengonzaga.com>
2025-09-20 10:09:42 +0000

    tweak: revise the submit idea URL with params

--- src/pages/Ideas.tsx
+++ src/pages/Ideas.tsx
@@ -152,7 +152,7 @@ const Ideas: React.FC = () => {
 
   const handleSubmitIdea = () => {
     const githubUrl =
-      'https://github.com/bettergovph/bettergov/issues/new/choose'
+      'https://github.com/bettergovph/bettergov/issues/new?assignees=&labels=enhancement%2Cidea&projects=&template=idea-submission.md&title=%5BIDEA%5D+'
     window.open(githubUrl, '_blank')
   }
 
